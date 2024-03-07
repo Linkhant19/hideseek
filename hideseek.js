08de2452a73447c04e81d22d199b8f4d3fbcaf11
Lin Khant Ko <lkk19@bu.edu>
2024-03-06 21:08:34 -0500

    Very backbone functionality of the game is working.

--- hideseek.js
+++ hideseek.js
@@ -4,7 +4,9 @@ var hiderReveal = false; //if Seek and Reveal gets played in the same turn, hide
 var hidden;
 var seekerdeck;
 var hiderdeck;
+var hiderCard;
 
+var hideTurns = 2;
 var canHide = true;
 
 window.onload = function() {
@@ -12,6 +14,7 @@ window.onload = function() {
     buildHiderDeck();
     shuffleDeck(seekerdeck)
     shuffleDeck(hiderdeck)
+    startGame();
 }
 
 function buildSeekerDeck() {
@@ -31,6 +34,65 @@ function shuffleDeck(deck) {
     }
     console.log(deck);
 }
+
+function startGame() {
+    hidden = seekerdeck.pop();
+    if (hidden == "Seek") {
+        seekerSeek = true;
+    }
+    console.log("Seeker drew", hidden)
+    let cardImg = document.createElement("img");
+    let card = hidden;
+    cardImg.src = "./cards/" + card + ".png";
+
+    document.getElementById("stand").addEventListener("click", stand);
+    document.getElementById("hide").addEventListener("click", hide)
+
+}
+
+function stand() {
+    hideTurns = 2;
+    let cardImg = document.createElement("img");
+    hiderCard = hiderdeck.pop();
+    cardImg.src = "./cards/" + hiderCard + ".png";
+    document.getElementById("hidercard").src = "./cards/" + hiderCard + ".png";
+    hiderReveal = true;
+
+    compare();
+}
+
+function hide() {
+    if (!canHide) {
+        console.log ("You can't hide for more than 2 consecutive turns, Coward!")
+        return
+    }
+    hiderReveal = false;
+    hiderCard = "Hide";
+
+    compare();
+}
+
+function compare() {
+    document.getElementById("hidden").src = "./cards/" + hidden + ".png";
+
+    let message = "";
+    if (seekerSeek && hiderReveal) {
+        message = "Hider Lose!";
+    }
+    else {
+        message = "Continue";
+        startGame();
+    }
+
+    document.getElementById("seeker-card").innerText = hidden;
+    document.getElementById("hider-card").innerText = hiderCard;
+    document.getElementById("results").innerText = message;
+}
+
+
+
+
+
 
 
 
