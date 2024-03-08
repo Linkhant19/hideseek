67b1386894e9e5e99160cb0be32941b3aa237ed4
Lin Khant Ko <lkk19@bu.edu>
2024-03-08 10:01:30 -0500

    Implemented a draft of selection page and connected to main game page.

---
+++ selection.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById("MotherChick").addEventListener("click", loadNextPage);
+}
+
+function loadNextPage() {
+    window.location.href = "index.html";
+}
